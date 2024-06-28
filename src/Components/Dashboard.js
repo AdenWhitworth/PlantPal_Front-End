e3056dccfd66e830392db8b87f76ee5614bc61e5
AdenWhitworth <aden.whitworth@gmail.com>
2024-06-28 09:15:14 -0700

    added mui gauge to show moisture level

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -6,10 +6,10 @@ import DeviceItem from "../Components/DeviceItem";
 import InputField from "../Components/InputField";
 import glass from "../Images/glass-brown.svg";
 import plus from "../Images/plus-circle-green.svg";
+import { Gauge, gaugeClasses  } from '@mui/x-charts/Gauge';
 
 export default function Dashboard({}) {
 
-    
     return (
         <section className="dashboard">
             <div className='dashboard-grid'>
@@ -55,8 +55,44 @@ export default function Dashboard({}) {
                     </div>
 
                 </div>
-                <div className='dashboard-moisture'></div>
-                <div className='dashboard-connection'></div>
+
+                <div className='dashboard-moisture'>
+                    
+                    <h3>Plant Moisture Level</h3>
+
+                    <div className='moisture-gauge'>
+                        <Gauge
+                            className='moisture-gauge'
+                            value={40}
+                            startAngle={-90}
+                            endAngle={90}
+                            innerRadius="60%"
+                            outerRadius="100%"
+                            cornerRadius="20%"
+                            sx={{
+                                [`& .${gaugeClasses.valueText}`]: {
+                                    display: 'none',
+                                },
+                                [`& .${gaugeClasses.valueArc}`]: {
+                                    fill: '#C1E899',
+                                },
+                                [`& .${gaugeClasses.referenceArc}`]: {
+                                    fill: '#D9D9D9',
+                                },
+                            }}
+                        />
+                    </div>
+
+                    <div className='moisture-labels'>
+                        <h4>Dry</h4>
+                        <h4>Wet</h4>
+                    </div>
+                    
+                </div>
+
+                <div className='dashboard-connection'>
+                    
+                </div>
                 <div className='dashboard-status'></div>
                 <div className='dashboard-automate'></div>
             </div>
