2344618b5221610b98b46c04560a8e98b97c7540
AdenWhitworth <aden.whitworth@gmail.com>
2024-09-02 13:20:04 -0400

    updated add device modal to use modal component

--- src/Modals/AddDeviceModal.js
+++ src/Modals/AddDeviceModal.js
@@ -1,8 +1,7 @@
 import React, {useEffect} from 'react';
-import x_circle from '../Images/x-circle-black.svg';
+import Modal from '../Components/Modal';
 import time from '../Images/time-green.svg';
 import plus from "../Images/plus-circle-green.svg";
-import Button from '../Components/Button';
 import { useDevice } from '../Provider/DeviceProvider';
 
 export default function AddDeviceModal({setConnectDeviceToggle}) {
@@ -18,36 +17,33 @@ export default function AddDeviceModal({setConnectDeviceToggle}) {
     }
 
     return (
-        <div className="modal">
-            <div className='modal-content'>
-                <div className='modal-close'>
-                    <img className='grow' src={x_circle} alt='Close Icon' onClick={handleCloseClick}></img>
-                </div>
 
-                {device.presence_connection? <h4>Successfully to {device.cat_num}</h4> : <h4>Connecting to {device.cat_num}...</h4>}
+        <Modal 
+            addClose={true} 
+            addButton={device.presence_connection}
+            buttonLabel='Continue'
+            styleType='primary'
+            children={
+                <>
+                    {device.presence_connection? <h4>Successfully to {device.cat_num}</h4> : <h4>Connecting to {device.cat_num}...</h4>}
 
-                {device.presence_connection? 
-                    <img src={plus} alt='Plus Icon'></img>
-                :
-                    <div className='modal-time'>
-                        <img className='flip-image' src={time} alt='Time Icon'></img>
-                    </div>
-                }
-                
-                {device.presence_connection?
-                    <></>
+                    {device.presence_connection? 
+                        <img src={plus} alt='Plus Icon'></img>
                     :
-                    <p>Press the network button on the device to connect. Led on device will turn green when successful.</p>
-                }    
-
-                {device.presence_connection? 
-                    <div className='modal-btns'>
-                        <Button styleType='primary' onClick={handleContinueClick}>Continue</Button>
-                    </div>
-                    :
-                    <></>
-                }
-            </div>
-        </div>
+                        <div className='modal-time'>
+                            <img className='flip-image' src={time} alt='Time Icon'></img>
+                        </div>
+                    }
+                    
+                    {device.presence_connection?
+                        <></>
+                        :
+                        <p>Press the network button on the device to connect. Led on device will turn green when successful.</p>
+                    }
+                </>
+            }
+            handleCloseClick={handleCloseClick}
+            handleButtonClick={handleContinueClick}
+        ></Modal>
     );
 }
