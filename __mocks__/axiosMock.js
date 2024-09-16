bbfc931323a30b9c82d3676d553495022431a7ab
AdenWhitworth <aden.whitworth@gmail.com>
2024-09-15 22:03:52 -0400

    updated axios mock and apiclient bug

--- __mocks__/axiosMock.js
+++ __mocks__/axiosMock.js
@@ -1,13 +1,24 @@
 const axios = {
-    defaults: {
-      headers: {
-        common: {},
+  create: jest.fn(function (config) {
+    return {
+      ...axios,
+      defaults: { ...config },
+      interceptors: {
+        response: {
+          use: jest.fn(),
+        },
       },
+    };
+  }),
+  defaults: {
+    headers: {
+      common: {},
     },
-    get: jest.fn(() => Promise.resolve({ data: {} })),
-    post: jest.fn(() => Promise.resolve({ data: {} })),
-    put: jest.fn(() => Promise.resolve({ data: {} })),
-    delete: jest.fn(() => Promise.resolve({ data: {} })),
+  },
+  get: jest.fn(() => Promise.resolve({ data: {} })),
+  post: jest.fn(() => Promise.resolve({ data: {} })),
+  put: jest.fn(() => Promise.resolve({ data: {} })),
+  delete: jest.fn(() => Promise.resolve({ data: {} })),
 };
-  
+
 export default axios;
