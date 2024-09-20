b5ac87e7ed937492e62f073080508295eae0297e
AdenWhitworth <aden.whitworth@gmail.com>
2024-09-20 16:03:39 -0400

    adding missing files

---
+++ src/Containers/Dashboard/Auto/Auto.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { axisClasses } from '@mui/x-charts/ChartsAxis';
+import { BarChart } from '@mui/x-charts/BarChart';
+import './Auto.css';
+
+const chartSetting = {
+    yAxis: [
+        {
+            label: 'Times Watered',
+        },
+    ],
+    series: [
+        { 
+            type: 'bar' as 'bar',
+            dataKey: 'times', 
+            valueFormatter: (value: number | null) => value !== null ? `${value}x` : 'N/A', 
+            color: '#C1E899' 
+        }
+    ],
+    sx: {
+        [`& .${axisClasses.directionY} .${axisClasses.label}`]: {
+            transform: 'translateX(-10px)',
+        },
+    },
+};
+
+interface WaterOccurrence {
+    date: string;
+    times: number;
+}
+
+interface AutoProps {
+    waterOccurance: WaterOccurrence[];
+}
+
+export default function Auto({
+    waterOccurance
+}:AutoProps) {
+
+    const dataset = waterOccurance.map((occurrence) => ({
+        date: occurrence.date,
+        times: occurrence.times,
+    }));
+
+    return (
+        <div className='auto'>
+            <BarChart
+                dataset={dataset}
+                xAxis={[
+                    { 
+                        scaleType: 'band', 
+                        dataKey: 'date', 
+                        tickPlacement:'middle', 
+                        tickLabelPlacement:'middle'
+                    },
+                ]}
+                {...chartSetting}
+                borderRadius={5}
+            />
+        </div>
+    );
+}
